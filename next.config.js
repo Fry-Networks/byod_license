a8271c0d5ee874d7b6d93aaf4f109153a727a4fc
Simon <44496148+siim0n@users.noreply.github.com>
2023-06-23 11:51:36 +0200

    test

--- next.config.js
+++ next.config.js
@@ -5,7 +5,42 @@ const nextConfig = {
     },
     experimental: {
         serverActions: true,
+    },
+    headers: async () => {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: ContentSecurityPolicy.replace(/\n/g, ''),
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'camera=(), microphone=(), geolocation=()',
+                    },
+                ],
+            },
+        ]
     }
+    
 }
+
+const ContentSecurityPolicy = `
+  font-src 'self' js.stripe.com;
+  
+`
 
 module.exports = nextConfig
