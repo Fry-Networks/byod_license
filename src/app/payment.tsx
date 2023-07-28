39c14133ef7a99075c09d0c8698b1d1ecd054fe3
Simon <44496148+siim0n@users.noreply.github.com>
2023-07-28 10:51:24 +0200

    update walletconnect

--- src/app/payment.tsx
+++ src/app/payment.tsx
@@ -2,20 +2,49 @@
 import React, { useEffect } from 'react';
 import {
     reconnectProviders,
-    initializeProviders,
+    useInitializeProviders,
     WalletProvider,
+    PROVIDER_ID,
+    algosigner,
+    useWallet
 } from "@txnlab/use-wallet";
 import Connect from "./components/Connect";
 import Transact from "./components/Transact";
+import { DeflyWalletConnect } from '@blockshake/defly-connect'
+import { PeraWalletConnect } from '@perawallet/connect'
+import { DaffiWalletConnect } from '@daffiwallet/connect'
+import MyAlgoConnect from '@randlabs/myalgo-connect';
+import { WalletConnectModalSign } from '@walletconnect/modal-sign-html'
 
-const walletProviders = initializeProviders();
+
 
 export default function Payment() {
-
+    const walletProviders = useInitializeProviders({
+        providers: [
+            { id: PROVIDER_ID.DEFLY, clientStatic: DeflyWalletConnect },
+            { id: PROVIDER_ID.PERA, clientStatic: PeraWalletConnect },
+            { id: PROVIDER_ID.DAFFI, clientStatic: DaffiWalletConnect },
+            { id: PROVIDER_ID.MYALGO, clientStatic: MyAlgoConnect },
+            {
+                id: PROVIDER_ID.WALLETCONNECT,
+                clientStatic: WalletConnectModalSign,
+                clientOptions: {
+                    projectId: '74761852c2f607c540bb116a1bc9f011',
+                    metadata: {
+                        name: 'Example Dapp',
+                        description: 'Example Dapp',
+                        url: '#',
+                        icons: ['https://walletconnect.com/walletconnect-logo.png']
+                    }
+                }
+            }
+        ]
+    })
     useEffect(() => {
-        reconnectProviders(walletProviders);
+        if (walletProviders !== null) {
+            reconnectProviders(walletProviders);
+        }
     }, []);
-
     return (
         <div
             style={{
