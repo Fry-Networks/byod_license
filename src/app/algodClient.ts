47389037004456d209d9007e0b833ac07c911770
Simon <44496148+siim0n@users.noreply.github.com>
2023-12-27 12:23:55 +0100

    revert

--- src/app/algodClient.ts
+++ src/app/algodClient.ts
@@ -1,9 +1,16 @@
 import algosdk from "algosdk";
 import { DEFAULT_NODE_BASEURL, DEFAULT_NODE_PORT, DEFAULT_NODE_TOKEN } from "@txnlab/use-wallet";
+/*
 const algodClient = new algosdk.Algodv2(
   DEFAULT_NODE_TOKEN,
   "https://xna-mainnet-api.algonode.cloud/",
   443
+);
+*/
+const algodClient = new algosdk.Algodv2(
+  DEFAULT_NODE_TOKEN,
+  DEFAULT_NODE_BASEURL,
+  DEFAULT_NODE_PORT
 );
 
 export default algodClient;
