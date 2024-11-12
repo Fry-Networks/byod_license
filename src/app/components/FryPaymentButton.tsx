221b340475c033637b8c0bc34f5f901ff8cd4c1f
yoyumiracle <157239347+yoyumiracle@users.noreply.github.com>
2024-11-13 01:21:38 +0900

    make it to pay fully with fry

--- src/app/components/FryPaymentButton.tsx
+++ src/app/components/FryPaymentButton.tsx
@@ -1,12 +1,26 @@
-import React, { useContext, useState } from 'react';
-import { MessagesContext } from './Transact';
-const FryPaymentButton = ({ sendTransaction, from, email, isPaid, enabled }: { sendTransaction: (from: string, email: string) => Promise<void>, from: string, email: string, isPaid: boolean, enabled: boolean }) => {
+import React, { useContext, useState } from "react";
+import { MessagesContext } from "./Transact";
+const FryPaymentButton = ({
+  sendTransaction,
+  from,
+  email,
+  isPaid,
+  enabled,
+}: {
+  sendTransaction: (from: string, email: string) => Promise<void>;
+  from: string;
+  email: string;
+  isPaid: boolean;
+  enabled: boolean;
+}) => {
   // Introduce a loading state
   const [isLoading, setIsLoading] = useState(false);
   const context = useContext(MessagesContext);
 
   if (!context) {
-    throw new Error("ChildComponent must be used within a TransactionMessageProvider");
+    throw new Error(
+      "ChildComponent must be used within a TransactionMessageProvider"
+    );
   }
 
   const { messages, setTransactionMessages } = context;
@@ -23,46 +37,44 @@ const FryPaymentButton = ({ sendTransaction, from, email, isPaid, enabled }: { s
         ...messages,
         fry: {
           message: error.message,
-          color: "#f00"
-        }
+          color: "#f00",
+        },
       });
       setIsLoading(false);
     }
     setIsLoading(false);
-  }
-  const condition = (!enabled || isLoading)
+  };
+  const condition = !enabled || isLoading;
   if (!isPaid) {
     return (
       <button
         onClick={async () => await handleTransaction(from, email)}
         style={{
           ...buttonStyle,
-          backgroundColor: enabled && !isLoading ? 'yellow' : 'grey',
-          cursor: enabled && !isLoading ? 'pointer' : 'default',
-          display: (!enabled) ? 'none' : 'inline-block'
+          backgroundColor: enabled && !isLoading ? "yellow" : "grey",
+          cursor: enabled && !isLoading ? "pointer" : "default",
+          display: !enabled ? "none" : "inline-block",
         }}
         disabled={condition && !isPaid}
         hidden={condition}
-
       >
-        {isLoading ? 'Processing...' : '$FRY Payment (52,50$ USD)'}
+        {isLoading ? "Processing..." : "$FRY Payment (103.00$ USD)"}
       </button>
     );
-  }
-  else return null;
+  } else return null;
 };
 
 const buttonStyle = {
-  backgroundColor: 'yellow',
-  border: 'none',
-  color: 'black',
-  padding: '15px 32px',
-  textDecoration: 'none',
-  display: 'inline-block',
-  fontSize: '16px',
-  margin: '4px 2px',
-  cursor: 'pointer',
-  borderRadius: '5px',
+  backgroundColor: "yellow",
+  border: "none",
+  color: "black",
+  padding: "15px 32px",
+  textDecoration: "none",
+  display: "inline-block",
+  fontSize: "16px",
+  margin: "4px 2px",
+  cursor: "pointer",
+  borderRadius: "5px",
 };
 
 export default FryPaymentButton;
--- src/app/components/PaymentModal.tsx
+++ src/app/components/PaymentModal.tsx
@@ -1,207 +1,296 @@
-'use client';
+"use client";
 import React, { useContext, useEffect, useState } from "react";
-import Modal from 'react-modal';
-import { repayLicense, getUserData, createUser, isUser, setUser } from '../classes/LicenseProcessor';
-import AlgoPaymentButton from './AlgoPaymentButton';
-import FryPaymentButton from './FryPaymentButton';
+import Modal from "react-modal";
+import {
+  repayLicense,
+  getUserData,
+  createUser,
+  isUser,
+  setUser,
+} from "../classes/LicenseProcessor";
+import AlgoPaymentButton from "./AlgoPaymentButton";
+import FryPaymentButton from "./FryPaymentButton";
 import Cross from "../assets/cross";
 import Check from "../assets/check";
-import { SplitPaymentModalProps } from '../types';
-import { CSSTransition } from 'react-transition-group';
+import { SplitPaymentModalProps } from "../types";
+import { CSSTransition } from "react-transition-group";
 import { MessagesContext, PaymentSuccessfulContext } from "./Transact";
-require('dotenv').config();
+require("dotenv").config();
 
-const PaymentMethod = ({ title, isPaid }: { title: string, isPaid: boolean }) => (
-    <div style={flexContainerStyle}>
-        <h3 style={titleStyle}>{title}</h3>
-        {isPaid ? <Check width={50} height={50} /> : <Cross width={50} height={50} />}
-    </div>
+const PaymentMethod = ({
+  title,
+  isPaid,
+}: {
+  title: string;
+  isPaid: boolean;
+}) => (
+  <div style={flexContainerStyle}>
+    <h3 style={titleStyle}>{title}</h3>
+    {isPaid ? (
+      <Check width={50} height={50} />
+    ) : (
+      <Cross width={50} height={50} />
+    )}
+  </div>
 );
 interface AnimatedElementsProps {
-    children: React.ReactNode;
-    inProp: boolean;
+  children: React.ReactNode;
+  inProp: boolean;
 }
 
-const AnimatedElements: React.FC<AnimatedElementsProps> = ({ inProp, children }) => (
-    <CSSTransition in={!inProp} timeout={300} classNames="card" unmountOnExit>
-        {children}
-    </CSSTransition>
+const AnimatedElements: React.FC<AnimatedElementsProps> = ({
+  inProp,
+  children,
+}) => (
+  <CSSTransition in={!inProp} timeout={300} classNames="card" unmountOnExit>
+    {children}
+  </CSSTransition>
 );
 
-const SplitPaymentModal = ({ modalIsOpen, closeModal, activeAddress, email, sendAlgoTransaction, sendFryTransaction, valid }: SplitPaymentModalProps) => {
-    const context = useContext(PaymentSuccessfulContext);
-    const messagesContext = useContext(MessagesContext);
-    if (!context) {
-        throw new Error("ChildComponent must be used within a PaymentSuccessfulProvider");
-    }
-    if (!messagesContext) {
-        throw new Error("ChildComponent must be used within a TransactionMessageProvider");
-    }
-
-    const { paymentSuccessful, setPaymentSuccessful } = context;
-    const { messages, setTransactionMessages } = messagesContext;
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        if (valid && modalIsOpen) {
-            const fetchUser = async () => {
-                if (!(await isUser(email))) {
-                    const user  = await createUser(email)
-                    setUser(user);
-                }
-                const result = await getUserData(email);
-                setPaymentSuccessful(result);
-                setTimeout(() => setIsLoading(false), 1000); // Set loading to false after 1 second
-            };
-           
-
-            fetchUser().catch((err) => {
-                console.log(err);
-            });
-
-            
-        }
-    }, [email, modalIsOpen]);
-
-    return (
-        <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            style={modalStyles}
-            contentLabel="Split Payment Modal"
-        >
-            <h1 style={headerStyle}>Payment</h1>
-            <p style={{ textAlign: 'center' }}>
-                You will have to pay 52,50$ USD (in $ALGO) and 52,50$ USD (in $FRY) using your wallet.
-                If you see an error, please contact us right away, and don't try to pay again.
-                <br />
-               <strong
-               style={{color: 'red', fontSize: '20px'}}
-                >It is HIGHLY recommended to browse this website using a computer, as mobile devices are known to have issues with the transaction system we're using (the responsible team is working on it)</strong>
-            </p>
-            <div style={{ display: 'flex', gap: '10px', justifyContent: 'space-between', position: 'relative' }}>
-                <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <PaymentMethod title="Algo Payment" isPaid={paymentSuccessful.algo} />
-
-                    <p style={{ marginTop: '10px', marginBottom: '10px', textAlign: 'center', color: messages.algo.color, boxSizing: "border-box" }}>{messages.algo.message}</p>
-                    {isLoading ? <p>Loading...</p> : <AlgoPaymentButton sendTransaction={sendAlgoTransaction} from={activeAddress} email={email} isPaid={paymentSuccessful.algo} />}
-
-                </div>
-
-                <div style={{ width: '4px', backgroundColor: 'red' }} />
-
-
-
-
-                <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <PaymentMethod title="Fry Payment" isPaid={paymentSuccessful.fry} />
-                    <p style={{ marginTop: '10px', marginBottom: '10px', textAlign: 'center', color: messages.fry.color }}>{messages.fry.message}</p>
-                    {isLoading ? <p>Loading...</p> : <FryPaymentButton sendTransaction={sendFryTransaction} from={activeAddress} email={email} isPaid={paymentSuccessful.fry} enabled={paymentSuccessful.algo} />}
-                </div>
-            </div>
-
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                <button
-                    onClick={
-                        async () => {
-                            const payAgain = await repayLicense(email);
-                            if (payAgain) {
-
-                                setPaymentSuccessful({
-                                    algo: false,
-                                    fry: false
-                                });
-
-                                setTransactionMessages({
-                                    algo: {
-                                        message: "",
-                                        color: "#000"
-                                    }, fry: {
-                                        message: "",
-                                        color: "#000"
-                                    }
-                                })
-                            }
-                            else {
-                                setTransactionMessages({
-                                    algo: {
-                                        message: "",
-                                        color: "#000"
-                                    }, fry: {
-                                        message: "You can't pay for another license at the moment...",
-                                        color: "#FF0000"
-                                    }
-                                })
-                            }
-                        }
-                    }
-                    style={{
-                        ...buttonStyle,
-                        backgroundColor: !paymentSuccessful.fry ? '#CCCCCC' : 'yellow',
-                        display: !paymentSuccessful.fry ? 'none' : 'block',
-                    }}
-                    hidden={!paymentSuccessful.fry}
-                    disabled={!paymentSuccessful.fry}
-
-                >
-                    Buy another license
-                </button>
-
-
-                <button onClick={
-                    () => {
-                        closeModal();
-                        setTransactionMessages({
-                            algo: {
-                                message: "",
-                                color: "#000"
-                            }, fry: {
-                                message: "",
-                                color: "#000"
-                            }
-                        })
-
-
-                    }
-                } style={buttonStyle}>Close</button>
-
-
-
-            </div>
-        </Modal>
-
-
+const SplitPaymentModal = ({
+  modalIsOpen,
+  closeModal,
+  activeAddress,
+  email,
+  sendAlgoTransaction,
+  sendFryTransaction,
+  valid,
+}: SplitPaymentModalProps) => {
+  const context = useContext(PaymentSuccessfulContext);
+  const messagesContext = useContext(MessagesContext);
+  if (!context) {
+    throw new Error(
+      "ChildComponent must be used within a PaymentSuccessfulProvider"
     );
+  }
+  if (!messagesContext) {
+    throw new Error(
+      "ChildComponent must be used within a TransactionMessageProvider"
+    );
+  }
+
+  const { paymentSuccessful, setPaymentSuccessful } = context;
+  const { messages, setTransactionMessages } = messagesContext;
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    if (valid && modalIsOpen) {
+      const fetchUser = async () => {
+        if (!(await isUser(email))) {
+          const user = await createUser(email);
+          setUser(user);
+        }
+        const result = await getUserData(email);
+        setPaymentSuccessful(result);
+        setTimeout(() => setIsLoading(false), 1000); // Set loading to false after 1 second
+      };
+
+      fetchUser().catch((err) => {
+        console.log(err);
+      });
+    }
+  }, [email, modalIsOpen]);
+
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      style={modalStyles}
+      contentLabel="Split Payment Modal"
+    >
+      <h1 style={headerStyle}>Payment</h1>
+      <p style={{ textAlign: "center" }}>
+        You will have to pay 103$ USD (in $FRY) using your wallet. If you see an
+        error, please contact us right away, and don't try to pay again.
+        <br />
+        <strong style={{ color: "red", fontSize: "20px" }}>
+          It is HIGHLY recommended to browse this website using a computer, as
+          mobile devices are known to have issues with the transaction system
+          we're using (the responsible team is working on it)
+        </strong>
+      </p>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "center",
+          position: "relative",
+        }}
+      >
+        {/* <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <PaymentMethod title="Algo Payment" isPaid={paymentSuccessful.algo} />
+
+          <p
+            style={{
+              marginTop: "10px",
+              marginBottom: "10px",
+              textAlign: "center",
+              color: messages.algo.color,
+              boxSizing: "border-box",
+            }}
+          >
+            {messages.algo.message}
+          </p>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <AlgoPaymentButton
+              sendTransaction={sendAlgoTransaction}
+              from={activeAddress}
+              email={email}
+              isPaid={paymentSuccessful.algo}
+            />
+          )}
+        </div> */}
+
+        {/* <div style={{ width: "4px", backgroundColor: "red" }} /> */}
+
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <PaymentMethod title="Fry Payment" isPaid={paymentSuccessful.fry} />
+          <p
+            style={{
+              marginTop: "10px",
+              marginBottom: "10px",
+              textAlign: "center",
+              color: messages.fry.color,
+            }}
+          >
+            {messages.fry.message}
+          </p>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <FryPaymentButton
+              sendTransaction={sendFryTransaction}
+              from={activeAddress}
+              email={email}
+              isPaid={paymentSuccessful.fry}
+              enabled={true}
+            />
+          )}
+        </div>
+      </div>
+
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <button
+          onClick={async () => {
+            const payAgain = await repayLicense(email);
+            if (payAgain) {
+              setPaymentSuccessful({
+                algo: false,
+                fry: false,
+              });
+
+              setTransactionMessages({
+                algo: {
+                  message: "",
+                  color: "#000",
+                },
+                fry: {
+                  message: "",
+                  color: "#000",
+                },
+              });
+            } else {
+              setTransactionMessages({
+                algo: {
+                  message: "",
+                  color: "#000",
+                },
+                fry: {
+                  message: "You can't pay for another license at the moment...",
+                  color: "#FF0000",
+                },
+              });
+            }
+          }}
+          style={{
+            ...buttonStyle,
+            backgroundColor: !paymentSuccessful.fry ? "#CCCCCC" : "yellow",
+            display: !paymentSuccessful.fry ? "none" : "block",
+          }}
+          hidden={!paymentSuccessful.fry}
+          disabled={!paymentSuccessful.fry}
+        >
+          Buy another license
+        </button>
+
+        <button
+          onClick={() => {
+            closeModal();
+            setTransactionMessages({
+              algo: {
+                message: "",
+                color: "#000",
+              },
+              fry: {
+                message: "",
+                color: "#000",
+              },
+            });
+          }}
+          style={buttonStyle}
+        >
+          Close
+        </button>
+      </div>
+    </Modal>
+  );
 };
 
 const modalStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        borderRadius: '20px',
-        color: 'black',
-        transition: 'opacity 0.4s'
-    },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "20px",
+    color: "black",
+    transition: "opacity 0.4s",
+  },
 };
 
 const buttonStyle = {
-    backgroundColor: 'yellow',
-    border: 'none',
-    color: 'black',
-    padding: '15px 32px',
-    textDecoration: 'none',
-    display: 'inline-block',
-    fontSize: '16px',
-    margin: '4px 2px',
-    cursor: 'pointer',
-    borderRadius: '5px',
+  backgroundColor: "yellow",
+  border: "none",
+  color: "black",
+  padding: "15px 32px",
+  textDecoration: "none",
+  display: "inline-block",
+  fontSize: "16px",
+  margin: "4px 2px",
+  cursor: "pointer",
+  borderRadius: "5px",
 };
 
-const headerStyle = { textAlign: 'center', fontSize: '20px', fontWeight: 'bold' } as const;
-const flexContainerStyle = { display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' } as const;
-const titleStyle = { marginRight: '10px' } as const;
+const headerStyle = {
+  textAlign: "center",
+  fontSize: "20px",
+  fontWeight: "bold",
+} as const;
+const flexContainerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+} as const;
+const titleStyle = { marginRight: "10px" } as const;
 
 export default SplitPaymentModal;
